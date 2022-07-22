aabc89be4f228ab8b393a61fb96fd2092c13d4eb
NewJJFromHK <kevin518ca@gmail.com>
2022-07-22 19:51:39 -0400

    user.ts added in Models and PassportLocalSchema fixed

---
+++ .history/Server/Config/db_20220722012527.ts
@@ -0,0 +1,4 @@
+//export const LocalURI = "mongodb://127.0.0.1/books229";
+export const RemoteURI = "mongodb+srv://surwe:NmA9XDSXzeYDLzUx@cluster0.n88v6.mongodb.net/Group?retryWrites=true&w=majority"
+export const HostName = "Cloudmongodb";
+export const Secret = "someSecret";
