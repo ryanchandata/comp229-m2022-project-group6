a6ae171fcf02809eb21a185d9bcd29aa1117ecaa
Ryan Chan <ryan@ryanchan.info>
2022-07-27 01:21:32 -0500

    Config the server

--- Server/Models/survey.js
+++ Server/Models/survey.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const Schema = mongoose_1.default.Schema;
 const surveySchema = new Schema({
-    SurveyID: , String,
+    SurveyID: String,
     user: String,
     name: String,
     dateCreated: {
