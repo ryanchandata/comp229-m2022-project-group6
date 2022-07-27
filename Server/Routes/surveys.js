6b502e061e35bb2138d5dacd742dd07ddad61469
Ryan Chan <ryan@ryanchan.info>
2022-07-27 01:17:55 -0500

    Re-config the MongoDB

--- Server/Routes/surveys.js
+++ Server/Routes/surveys.js
@@ -8,7 +8,7 @@ const router = express_1.default.Router();
 const survey_1 = require("../Controllers/survey");
 router.get('/survey', survey_1.DisplaySurveyList);
 router.get('/add', survey_1.DisplayAddPage);
-router.get('/edit/:id', survey_1.DisplayEditPage);
+router.post('/edit/:id', survey_1.DisplayEditPage);
 router.post('/add', survey_1.ProcessAddPage);
 router.post('/edit/:id', survey_1.ProcessEditPage);
 router.post('/delete/:id', survey_1.ProcessDeletePage);
